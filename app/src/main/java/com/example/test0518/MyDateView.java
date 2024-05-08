5f55af8a8ff8d17be5f8017028f6b8942b5072ea
kid35648466 <111b13982@mailst.cjcu.edu.tw>
2024-05-08 14:15:49 +0800

    完成 SutDate calsss 設定

---
+++ app/src/main/java/com/example/test0518/MyDateView.java
@@ -0,0 +1,14 @@
+package com.example.test0518;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyDateView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_date_view);
+    }
+}
